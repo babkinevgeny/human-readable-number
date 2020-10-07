9e2b447bfa284230e7fc250a4e14587be1d37251
babkinevgeny <babkinevgeny@gmail.com>
2020-10-07 16:52:21 +0300

    feat: solve human readable number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,57 @@
+const dictionary = {
+    one: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
+    two: ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+    tens: ['ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety' ]
+}
+
 module.exports = function toReadable (number) {
-  
+    const str = number.toString();
+
+    if (str.length === 1) {
+        return dictionary.one[number]
+    }
+
+    function getSecondPart(string) {
+        const num = +string;
+        const firstDigit = +string[0];
+        const secondDigit = +string[1];
+
+        if (num % 10 === 0) {
+            return dictionary.tens[num / 10 - 1]
+        }
+
+        if (num < 10) {
+            return dictionary.one[num]
+        }
+
+        if (10 < num && num < 20) {
+            return dictionary.two[num - 10 - 1]
+        }
+
+        return `${dictionary.tens[firstDigit - 1]} ${dictionary.one[secondDigit]}`;
+    }
+
+    if (str.length === 2) {
+        return getSecondPart(str)
+    }
+
+    if (str.length === 3) {
+
+        const firstPart = `${dictionary.one[+str[0]]} hundred`;
+
+        if (number % 100 === 0) {
+            return firstPart;
+        }
+
+        const tale = str.substring(1);
+
+        const secondPart = getSecondPart(tale);
+
+        return `${firstPart} ${secondPart}`;
+
+    }
+
+    if (str.length > 3) {
+        return 'Sorry, task was only to convert three-digit number maximum'
+    }
 }
